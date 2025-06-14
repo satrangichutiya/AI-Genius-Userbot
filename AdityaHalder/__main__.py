7bccf5b57c038a4fdc46d8d32f8826e3f0f1e96a
satrangichutiya <gpyrogramxspam@gmail.com>
2025-06-14 19:13:35 +0530

    Update __main__.py

--- AdityaHalder/__main__.py
+++ AdityaHalder/__main__.py
@@ -1,34 +1,22 @@
 import asyncio, importlib
-
 from pytgcalls import idle
 
-from . import logs, plugs, vars
-from .plugins import ALL_PLUGINS
-from .modules.clients.clients import run_async_clients
-from .modules.clients.enums import run_async_enums
-from .modules.helpers.inline import run_async_inline
-from .modules.utilities.tgcalls import run_async_calls
+from AdityaHalder import logs, plugs, vars
+from AdityaHalder.plugins import ALL_PLUGINS
+from AdityaHalder.modules.clients.clients import run_async_clients
+from AdityaHalder.modules.clients.enums import run_async_enums
+from AdityaHalder.modules.helpers.inline import run_async_inline
+from AdityaHalder.modules.utilities.tgcalls import run_async_calls
 
 
 async def main():
     await run_async_clients()
     for all_plugin in ALL_PLUGINS:
-        imported_plugin = importlib.import_module(
-            "AdityaHalder.plugins" + all_plugin
-        )
-        if (hasattr
-            (
-                imported_plugin, "__NAME__"
-            ) and imported_plugin.__NAME__
-        ):
+        imported_plugin = importlib.import_module("AdityaHalder.plugins." + all_plugin)
+        if hasattr(imported_plugin, "__NAME__") and imported_plugin.__NAME__:
             imported_plugin.__NAME__ = imported_plugin.__NAME__
-            if (
-                hasattr(
-                    imported_plugin, "__MENU__"
-                ) and imported_plugin.__MENU__
-            ):
-                plugs[imported_plugin.__NAME__.lower()
-                ] = imported_plugin
+            if hasattr(imported_plugin, "__MENU__") and imported_plugin.__MENU__:
+                plugs[imported_plugin.__NAME__.lower()] = imported_plugin
     await run_async_enums()
     logs.info(">> Successfully Imported All Plugins.")
     await run_async_inline()
