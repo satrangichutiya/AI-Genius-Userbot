ce15f78f334a4feb3a0d5a05aef4bc31fb4768ca
satrangichutiya <gpyrogramxspam@gmail.com>
2025-06-14 20:01:50 +0530

    Update tgcalls.py

--- AdityaHalder/modules/utilities/tgcalls.py
+++ AdityaHalder/modules/utilities/tgcalls.py
@@ -1,14 +1,15 @@
-from pyrogram.types import Message
 from AdityaHalder.modules.clients.clients import call
 from .streams import run_stream, get_result, get_stream
 
 async def run_async_calls(chat_id, query, stream_type="Audio"):
+    # Get YouTube URL and thumbnail
     url, thumbnail = await get_result(query)
+
+    # Download audio/video file
     file = await get_stream(url, stream_type)
 
-    if stream_type == "Audio":
-        stream = await run_stream(file, "Audio")
-        await call.join_group_call(chat_id, stream)
-    elif stream_type == "Video":
-        audio, video = await run_stream(file, "Video")
-        await call.join_group_call(chat_id, audio, video)
+    # Prepare AudioPiped stream (video removed for compatibility)
+    stream = await run_stream(file, "Audio")
+
+    # Join group call with the audio stream
+    await call.join_group_call(chat_id, stream)
