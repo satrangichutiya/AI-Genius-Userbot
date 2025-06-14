e01a3699f947d59192e0b0f624e99108470f0e33
satrangichutiya <gpyrogramxspam@gmail.com>
2025-06-14 18:28:57 +0530

    Update streams.py

--- AdityaHalder/modules/utilities/streams.py
+++ AdityaHalder/modules/utilities/streams.py
@@ -21,64 +21,44 @@ async def run_async(func, *args, **kwargs):
 
 async def get_result(query: str):
     results = VideosSearch(query, limit=1)
-    for result in (await results.next())["result"]:
-        url = result["link"]
-        try:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        except:
-            thumbnail = USERBOT_PICTURE
+    result = (await results.next())["result"][0]
+    url = result["link"]
+    thumbnail = result.get("thumbnails", [{}])[0].get("url", USERBOT_PICTURE).split("?")[0]
     return url, thumbnail
 
 
 async def get_stream(link, stream_type):
-    if stream_type == "Audio":
-        ydl_opts = {
-            "format": "bestaudio/best",
-            "outtmpl": "downloads/%(id)s.%(ext)s",
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "quiet": True,
-            "no_warnings": True,
-        }
-    elif stream_type == "Video":
-        ydl_opts = {
-            "format": "(bestvideo[height<=?720][width<=?1280][ext=mp4])+(bestaudio[ext=m4a])",
-            "outtmpl": "downloads/%(id)s.%(ext)s",
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "quiet": True,
-            "no_warnings": True,
-        }
+    ydl_opts = {
+        "format": "bestaudio/best" if stream_type == "Audio" else "(bestvideo[height<=?720][width<=?1280][ext=mp4])+(bestaudio[ext=m4a])",
+        "outtmpl": "downloads/%(id)s.%(ext)s",
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "quiet": True,
+        "no_warnings": True,
+    }
 
-    x = yt_dlp.YoutubeDL(ydl_opts)
-    info = x.extract_info(link, False)
-    file = os.path.join("downloads", f"{info['id']}.{info['ext']}")
-    if os.path.exists(file):
-        return file
-    await run_async(x.download, [link])
-    return file
+    ydl = yt_dlp.YoutubeDL(ydl_opts)
+    info = ydl.extract_info(link, download=False)
+    file_path = os.path.join("downloads", f"{info['id']}.{info['ext']}")
+    if not os.path.exists(file_path):
+        await run_async(ydl.download, [link])
+    return file_path
 
 
 async def run_stream(file, stream_type):
+    audio = AudioPiped(
+        path=file,
+        parameters=AudioParameters(
+            bitrate=48000,
+            channels=2,
+        ),
+    )
+
     if stream_type == "Audio":
-        audio_stream = AudioPiped(
-            path=file,
-            parameters=AudioParameters(
-                bitrate=48000,
-                channels=2,
-            ),
-        )
-        return Stream(audio_stream)
+        return Stream(audio)
 
     elif stream_type == "Video":
-        audio_stream = AudioPiped(
-            path=file,
-            parameters=AudioParameters(
-                bitrate=48000,
-                channels=2,
-            ),
-        )
-        video_stream = VideoPiped(
+        video = VideoPiped(
             path=file,
             parameters=VideoParameters(
                 width=1280,
@@ -86,7 +66,7 @@ async def run_stream(file, stream_type):
                 frame_rate=30,
             ),
         )
-        return Stream(audio_stream, video_stream)
+        return Stream(audio, video)
 
 
 async def close_stream(chat_id):
@@ -96,5 +76,5 @@ async def close_stream(chat_id):
         pass
     try:
         return await call.leave_group_call(chat_id)
-    except:
+    except Exception:
         pass
